94d2b235705031e70ab5e20b50489d58ae7fab2c
Joao vitor <joaoguerra77@hotmail.com>
2025-08-29 14:12:13 -0300

    Implementaçao do home/banner/footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css'
+import Banner from './components/Banner'
+import Container from './components/Container'
 import Footer from './components/Footer'
 import Header from './components/Header'
 
@@ -6,6 +8,11 @@ function App() {
   return (
     <>
         <Header />
+        <Banner imagem="home" />
+        <Container>
+          <h1>Hello Word</h1>
+          <p>Ola Mundo! Estou aprendendo React JS!</p>
+        </Container>
         <Footer />
     </>
     
